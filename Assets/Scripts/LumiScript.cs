5333a426c7f3e7125b389727332ce89aeeb779c6
Jucielly silva <juciellysouzasilva@gmail.com>
2023-05-29 15:27:21 -0300

    lumi basic follow

--- Assets/Scripts/LumiScript.cs
+++ Assets/Scripts/LumiScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class LumiScript : MonoBehaviour
 {
+    [SerializeField]
+   private float _speed = 3.0f;
+    private Transform followTarget;
     // Start is called before the first frame update
     void Start()
     {
-        
+        followTarget = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector2.Distance(transform.position, followTarget.position) > 6)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, followTarget.position, _speed * Time.deltaTime);
+        }
+       
     }
 }
